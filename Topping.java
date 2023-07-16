2af42335d5fa40b3017114cc8de1186f339c2527
Jasmine <104976372+jasgayamo@users.noreply.github.com>
2023-07-16 19:16:20 +0800

    Topping.java

---
+++ Topping.java
@@ -0,0 +1,12 @@
+public class Topping {
+    
+    private String sTopping;
+
+    public Topping(String sTopping) {
+        this.sTopping = sTopping;
+    }
+
+    public String getToppingName() {
+        return sTopping;
+    }
+}
