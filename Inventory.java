e43475bb0cfbb84ac5d805940a833e33bf42766b
Jasmine <104976372+jasgayamo@users.noreply.github.com>
2023-07-19 20:29:25 +0800

    Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,54 +1,35 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Inventory {
     
-    private int nItemQty;
-    private int nToppingQty;
-    private ArrayList<Item> itemList;
-    private ArrayList<Topping> toppingList;
+    private int nProductQty;
+    private Map<Product, Integer> productList;
 
-    public Inventory(Item item, int nItemQty) {
-        this.itemList = new ArrayList<Item>();
-        this.toppingList = new ArrayList<Topping>();
-        this.itemList.add(item);
-        this.nItemQty = nItemQty;
+    public Inventory() {
+        productList = new HashMap<>();
     }
 
-    public int getItemQty() { 
-        return nItemQty;
-    }
+    public void restockItem(Product product, int nRestockQty) {
 
-    public int getToppingQty() {
-        return nToppingQty;
-    }
+        int nCurrentQty;
 
-    public void restockItem(Item item, int nRestockQty) {
-        
-        int nCurrentItemQty = getItemQty();
-        int nNewItemQty = nCurrentItemQty + nRestockQty;
-
-        if (nCurrentItemQty == 10) {
-            System.out.println("Restocking unsuccessful." + item.getItem() + " does not need restocking.\n");
-        } 
-        
-        else if (nCurrentItemQty < 10) {
-            nItemQty = nNewItemQty;
-            System.out.println("Restocking...\n" + item.getItem() + " restocked successfully.\n");
+        if (productList.containsKey(product)) {
+            nCurrentQty = productList.get(product);
+            productList.put(product, nCurrentQty + nRestockQty);
+        } else {
+            productList.put(product, nRestockQty);
         }
     }
-  
-    public void restockTopping(Topping topping, int nTopQty) {
 
-        int nCurrentTopQty = getToppingQty();
-        int nNewTopQty = nCurrentTopQty + nTopQty;
-    
-        if (nCurrentTopQty == 10) {
-           System.out.println("Restocking unsuccessful." + topping.getToppingName() + " does not need restocking.\n");
-        } 
-        
-        else if (nCurrentTopQty < 10) {
-            nToppingQty = nNewTopQty;
-            System.out.println("Restocking...\n" + topping.getToppingName() + " restocked successfully.\n");
+    public void restockTopping(Product product, int nTopQty) {
+
+        int nCurrentQty;
+
+        if (productList.containsKey(product)) {
+            nCurrentQty = productList.get(product);
+            productList.put(product, nCurrentQty + nTopQty);
+        } else {
+            productList.put(product, nTopQty);
         }
     }
 
