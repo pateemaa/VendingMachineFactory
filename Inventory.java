32e5bcfb351cc1f31fecde31b51a1c7a86182e0f
Jasmine <104976372+jasgayamo@users.noreply.github.com>
2023-07-25 06:06:57 +0800

    Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,62 +1,66 @@
 import java.util.*;
 
 public class Inventory {
-    
-    private int nProductQty;
     private Map<Product, Integer> productList;
 
     public Inventory() {
         productList = new HashMap<>();
     }
 
-    public void restockItem(Product product, int nRestockQty) {
-
-        int nCurrentQty;
-
-        if (productList.containsKey(product)) {
-            nCurrentQty = productList.get(product);
-            productList.put(product, nCurrentQty + nRestockQty);
-        } else {
-            productList.put(product, nRestockQty);
+    public void displayInventory() {
+        System.out.println("Current Inventory:");
+        for (Product product : productList.keySet()) {
+            int quantity = productList.get(product);
+            System.out.println(product.getName() + ": " + quantity);
         }
     }
 
+    public void restockProduct(Product product, int nRestockQty) {
+        if (product instanceof Item) {
+            restockItem(product, nRestockQty);
+        } else if (product instanceof Topping) {
+            restockTopping(product, nRestockQty);
+        } else {
+            throw new IllegalArgumentException("Invalid Product Type");
+        }
+    }
+
+    public void reduceProduct(Product product, int nReduceQty) {
+        if (product instanceof Item) {
+            reduceItem(product, nReduceQty);
+        } else if (product instanceof Topping) {
+            reduceTopping(product, nReduceQty);
+        } else {
+            throw new IllegalArgumentException("Invalid Product Type");
+        }
+    }
+
+    public void restockItem(Product product, int nRestockQty) {
+        int nCurrentQty = productList.getOrDefault(product, 0);
+        productList.put(product, nCurrentQty + nRestockQty);
+    }
+
     public void reduceItem(Product product, int nReduceQty) {
-        int nCurrentQty, nNewQty;
-
-        nCurrentQty = productList.getOrDefault(product, 0);
-        nNewQty = nCurrentQty - nReduceQty;
-
+        int nCurrentQty = productList.getOrDefault(product, 0);
+        int nNewQty = nCurrentQty - nReduceQty;
         if (nNewQty < 0) {
-            nNewQty = 0;
+            nNewQty = 0; 
         }
         productList.put(product, nNewQty);
     }
 
     public void restockTopping(Product product, int nTopQty) {
-
-        int nCurrentQty;
-
-        if (productList.containsKey(product)) {
-            nCurrentQty = productList.get(product);
-            productList.put(product, nCurrentQty + nTopQty);
-        } else {
-            productList.put(product, nTopQty);
-        }
+        int nCurrentQty = productList.getOrDefault(product, 0);
+        productList.put(product, nCurrentQty + nTopQty);
     }
 
     public void reduceTopping(Product product, int nReduceQty) {
-
-        int nCurrentQty, nNewQty;
-
-        nCurrentQty = productList.getOrDefault(product, 0);
-        nNewQty = nCurrentQty - nReduceQty;
-
+        int nCurrentQty = productList.getOrDefault(product, 0);
+        int nNewQty = nCurrentQty - nReduceQty;
         if (nNewQty < 0) {
-            nNewQty = 0;
+            nNewQty = 0; 
         }
         productList.put(product, nNewQty);
-
     }
-
+    
 }
